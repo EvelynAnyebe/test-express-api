cbdd39c4971fa31cf3722e2ff2c862181d526e8c
Evelyn Anyebe <anyebeevelyn@gmail.com>
2021-04-27 09:26:52 +0100

    View single user

--- models/user.js
+++ models/user.js
@@ -1,43 +1,49 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
 const UserSchema = Schema(
-    {
-        userName: String,
-        firstName: String,
-        lastName: String,
-        otherNames: String,
-        email: String,
-        phone: String,
-        gender: String,
-        dob: Date,
-        countryOfResidence: String,
-        countryCode: String,
-        stateOfResidence: String,
-        cityOfResidence: String,
-        address: String,
-        role: {
-                user: Boolean,
-                tutor: Boolean,
-                admin: Boolean
-        },
-        password: String,
-        emailVerified: Boolean,
-        accountStatus: Boolean,
-        avatar: String,
-        auth: {
-            authtype: String,
-            accesstoken: {
-            token: String,
-            createdAt: String,
-            updatedAt: String,
-            refresh: String,
-            expired: Boolean
-            }
-        },
+  {
+    userName: String,
+    firstName: String,
+    lastName: String,
+    otherNames: String,
+    email: String,
+    phone: String,
+    gender: String,
+    dob: Date,
+    countryOfResidence: String,
+    countryCode: String,
+    stateOfResidence: String,
+    cityOfResidence: String,
+    address: String,
+    role: {
+      user: Boolean,
+      tutor: Boolean,
+      admin: Boolean,
     },
-    {timestamps: true}
+    password: String,
+    emailVerified: Boolean,
+    accountStatus: Boolean,
+    avatar: String,
+    auth: {
+      authtype: String,
+      accesstoken: {
+        token: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+        updatedAt: {
+          type: Date,
+          default: Date.now,
+        },
+        refresh: String,
+        expired: Boolean,
+      },
+    },
+  },
+  { timestamps: true }
 );
 
-export default model('user', UserSchema);
+export default model("user", UserSchema);
