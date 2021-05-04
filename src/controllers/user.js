1090ec49e6b271c9ff6306c5e24bd3a35fa3779a
Evelyn Anyebe <anyebeevelyn@gmail.com>
2021-05-04 17:40:39 +0100

    Change callback to async/await

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,44 +10,43 @@ import encrypt from '../utils/encrypt.js';
  */
 function prepareUser(body) {
   const { firstName, lastName, email, password } = body;
+  const emailVerificationToken = encrypt(firstName);
+  const role = { user: true };
+  const auth = { authtype: 'auth' };
   return {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-    emailVerificationToken: encrypt(firstName),
-    role: {
-      user: true,
-    },
-    auth: {
-      authtype: 'auth',
-    },
+    firstName,
+    lastName,
+    email,
+    password,
+    emailVerificationToken,
+    role,
+    auth,
   };
 }
 
 // Select attribute to return
 function selectField(newUser) {
- return (({
-  userName,
-  firstName,
-  lastName,
-  email,
-  role,
-  emailVerified,
-  emailVerificationToken,
-  accountStatus,
-  auth,
-}) => ({
-  userName,
-  firstName,
-  lastName,
-  email,
-  role,
-  emailVerified,
-  emailVerificationToken,
-  accountStatus,
-  auth,
-}))(newUser);
+  return (({
+    userName,
+    firstName,
+    lastName,
+    email,
+    role,
+    emailVerified,
+    emailVerificationToken,
+    accountStatus,
+    auth,
+  }) => ({
+    userName,
+    firstName,
+    lastName,
+    email,
+    role,
+    emailVerified,
+    emailVerificationToken,
+    accountStatus,
+    auth,
+  }))(newUser);
 }
 
 /*
@@ -57,9 +56,10 @@ export function validate(method) {
   switch (method) {
     case 'getUser': {
       return param('id', 'Expected identifier of length>=24')
-      .exists().isLength({
-        min: 24,
-      });
+        .exists()
+        .isLength({
+          min: 24,
+        });
     }
     case 'createUser': {
       return [
@@ -81,25 +81,28 @@ const fieldSelect =
   ' countryOfResidence countryCode stateOfResidence cityOfResidence ' +
   'address role emailVerified accountStatus avatar auth';
 
-//Get all users
-export function getUsers(req, res) {
-  User.find()
-    .select(fieldSelect)
-    .exec()
-    .then((user) => {
-      res.send({
+// Get all and return all users.
+export async function getUsers(req, res) {
+  try {
+    const users = await User.find().select(fieldSelect).exec();
+    if (!users.length) {
+      return res.send({
         statusCode: res.statusCode,
-        message: 'Successful',
-        data: user,
+        message: 'There are no registered users at the moment.',
+        data: users,
       });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        statusCode: res.statusCode,
-        message:
-          err.message || 'Some error occured while retrieving users. Try again',
-      });
+    }
+    res.send({
+      statusCode: res.statusCode,
+      message: 'Users returned successfuly.',
+      data: users,
     });
+  } catch (err) {
+    res.status(500).send({
+      statusCode: res.statusCode,
+      message: err.message || 'Some error occurred while retrieving users.',
+    });
+  }
 }
 
 /*
@@ -112,34 +115,33 @@ export function getUsers(req, res) {
  *
  *@apiSuccess (200) {Object} mixed User object
  */
-export function getUser(req, res) {
+export async function getUser(req, res) {
   /*
    *Finds the validation errors in this request and wraps
    *them in an object with handy functions
    *400 Bad Request error
    */
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).send({
-      statusCode: 400,
-      message: errors.array(),
-    });
-  }
-  //Validation passed, handle request
-  User.findById(req.params.id, fieldSelect).exec((err, user) => {
-    if (err) {
-      return res.send({
-        statusCode: 500,
-        message:
-          err.message || 'An internal error occurred. Try again in 15secs',
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({
+        statusCode: 400,
+        message: errors.array(),
       });
     }
+    //Validation passed, handle request
+    const user = await User.findById(req.params.id, fieldSelect).exec();
     res.send({
       statusCode: 200,
       message: 'successful',
       data: user,
     });
-  });
+  } catch (err) {
+    res.send({
+      statusCode: 500,
+      message: err.message || 'An internal error occurred. Try again in 15secs',
+    });
+  }
 }
 
 /*
@@ -156,7 +158,7 @@ export function getUser(req, res) {
  *
  *@apiSuccess (200) {Object} mixed User object
  */
-export function createUser(req, res) {
+export async function createUser(req, res) {
   try {
     //Finds the validation errors in this request
     const errors = validationResult(req);
@@ -168,32 +170,24 @@ export function createUser(req, res) {
     }
 
     //Check if email already exist
-    User.findOne({ email: req.body.email }, fieldSelect).exec((err, user) => {
-
-      //406 Not Acceptable
-      if (user) {
-        return res.status(406).send({
-          statusCode: 406,
-          message: 'User account already exist',
-          data: user,
-        });
-      }
-
-      //Create user
-      User.create(prepareUser(req.body), (err, newUser) => {
-        if (err) {
-          return res.status(500).send({
-            statusCode: 500,
-            message:
-              err.message || 'An internal error occurred. Try again in 15secs',
-          });
-        }
-        res.status(201).send({
-          statusCode: 201,
-          message: 'User created successfully',
-          data: selectField(newUser)
-        });
+    const user = await User.findOne({ email: req.body.email }, fieldSelect)
+    .exec();
+    //406 Not Acceptable
+    if (user) {
+      return res.status(406).send({
+        statusCode: 406,
+        message: 'User account already exist',
+        data: user,
       });
+    }
+
+    //Create user
+    const newUser = await User.create(prepareUser(req.body));
+
+    res.status(201).send({
+      statusCode: 201,
+      message: 'User created successfully',
+      data: selectField(newUser),
     });
   } catch (err) {
     return res.status(500).send({
